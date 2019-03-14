38827bff525e1b129c7076df0d8523eb56d8d0e9
Gustavo Melo <gustavo.melo_10@hotmail.com>
2019-03-14 14:24:57 -0300

    Projeto iniciado para aprendizado

---
+++ Promeiro projeto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Promeiro_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+
+            for (i=0; i==7; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(i.ToString());
+        }
+
+
+    }
+}
